a66d7c9c3975ccc4719c4bbade9b0e0101b77c8a
João Lucas <ylucasslalves@gmail.com>
2026-02-05 22:40:51 -0300

    app event startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,16 @@
 # Import's FastAPI
 from fastapi import FastAPI
 from routers import users
+from routers.users import create_db_and_tables
 
 app = FastAPI()
 
 app.include_router(users.router)
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/", tags=["main"])
 def root():
