e3975f505c4916320ff016f5122c75fc6bdb2a89
João Lucas <ylucasslalves@gmail.com>
2026-02-05 22:21:22 -0300

    mudando tipagem de age para int

--- backend/app/classes/user.py
+++ backend/app/classes/user.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, Field
 class UserBase(SQLModel):
     username: str = Field(index=True)
     full_name: str = Field(index=True)
-    age: str | None = Field(default=None)
+    age: int | None = Field(default=None)
 
 class User(UserBase):
     password: str
