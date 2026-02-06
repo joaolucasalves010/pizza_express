581bab5b954f447fb6fad6f6c2dda3026af9ccfb
João Lucas <ylucasslalves@gmail.com>
2026-02-05 22:41:11 -0300

    criando engine e session do banco de dados

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,14 +1,32 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Depends
 from fastapi.responses import JSONResponse, Response
 from typing import Annotated
 from classes.user import * # Importando todas as classes SQLMODEL
 
+from sqlmodel import SQLModel, Session, create_engine
+
 router = APIRouter()
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 fake_users_db = []
 
 @router.post("/users/", tags=["users"])
-def create_user(user: Annotated[User, Body()]) -> Response:
+def create_user(user: Annotated[User, Body()], sesion: SessionDep) -> Response:
+    
     db_user = user.model_dump()
     fake_users_db.append(db_user)
     return JSONResponse(status_code=200, content={"message": "Usuário criado com sucesso!"})
