062b702f43eab08fc37388ca00da8144482450c8
João Lucas <ylucasslalves@gmail.com>
2026-02-05 22:26:26 -0300

    criando user e adicionando no db fake

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,8 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
+from fastapi.responses import JSONResponse, Response
 from typing import Annotated
+from classes.user import * # Importando todas as classes SQLMODEL
 
 router = APIRouter()
 
-@router.post("/users/")
-def create_user():
-    return {"detail": "users"}
+fake_users_db = []
+
+@router.post("/users/", tags=["users"])
+def create_user(user: Annotated[User, Body()]) -> Response:
+    db_user = user.model_dump()
+    fake_users_db.append(db_user)
+    return JSONResponse(status_code=200, content={"message": "Usuário criado com sucesso!"})
+
+@router.get("/users/", status_code=200, tags=["users"])
+def read_users() -> list:
+    return fake_users_db
