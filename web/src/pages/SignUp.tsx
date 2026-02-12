3ea1971ec9965029a863b3ed3032186e55e2e2ca
João Lucas <ylucasslalves@gmail.com>
2026-02-12 00:24:11 -0300

    adicionando spinner e verificações

--- web/src/pages/SignUp.tsx
+++ web/src/pages/SignUp.tsx
@@ -1,8 +1,11 @@
 import { Pizza } from "lucide-react"
 import { Link } from "react-router-dom"
+import { Spinner } from "../components/Spinner"
+
 import api from "../services/api"
 
 import { useState } from "react"
+
 
 const SignUp = () => {
 
@@ -10,9 +13,38 @@ const SignUp = () => {
   const [fullName, setFullName] = useState("")
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
+  const [usernameExists, setUsernameExists] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = async(e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (password !== confirmPassword) {
+      setPasswordError(true)
+      return
+    }
+
+    try {
+      setIsLoading(true)
+      const res = await api.post("/users/", {
+        "username": username,
+        "full_name": fullName,
+        "password": password
+      })
+
+    } catch (err: any) {
+      if (err.response?.status === 409) {
+        setUsernameExists(true)
+      }
+
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
-    <form className="flex flex-col gap-2 md:max-w-[512px] w-full bg-gray-50 shadow-2xl shadow-gray-400 rounded-xl p-6">
+    <form className="flex flex-col gap-2 md:max-w-[512px] w-full bg-gray-50 shadow-2xl shadow-gray-400 rounded-xl p-6" onSubmit={handleSubmit}>
 
       <div>
         <div className="w-full flex items-center justify-center hover:transform hover:scale-110 transition-transform duration-200 cursor-pointer">
@@ -27,18 +59,25 @@ const SignUp = () => {
       <div className="flex flex-col w-full gap-2">
 
         <label htmlFor="username">Nome de usuário</label>
-        <input type="text" id="username" placeholder="joaolucasalves010" className="border p-2 rounded-xl outline-none"/>
+        <input type="text" id="username" placeholder="joaolucasalves010" className="border p-2 rounded-xl focus:outline-orange-500" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+        {usernameExists && <span className="text-red-500 font-semibold">Nome de usuário já existe!</span>}
 
         <label htmlFor="fullName">Nome completo</label>
-        <input type="text" id="fullName" placeholder="João Lucas Lima Alves" className="border p-2 rounded-xl outline-none"/>
+        <input type="text" id="fullName" placeholder="João Lucas Lima Alves" className="border p-2 rounded-xl focus:outline-orange-500" value={fullName} onChange={(e) => setFullName(e.target.value)} required/>
 
         <label htmlFor="password">Senha</label>
-        <input type="password" placeholder="******" className="border p-2 rounded-xl" />
+        <input type="password" placeholder="******" className="border p-2 rounded-xl focus:outline-orange-500" minLength={6} onChange={(e) => setPassword(e.target.value)} value={password} required />
 
         <label htmlFor="confirmPassword">Confirme sua senha</label>
-        <input type="password" id="confirmPassword" placeholder="******" className="border p-2 rounded-xl"/>    
-          
-        <button type="submit" className="bg-orange-500 rounded-xl p-2 text-white cursor-pointer hover:opacity-80 duration-200 ease-linear mt-5">Criar conta</button>
+        <input type="password" id="confirmPassword" placeholder="******" className="border p-2 rounded-xl focus:outline-orange-500" minLength={6} onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} required/>
+        {passwordError && <span className="text-red-500 font-semibold">As senhas não coincidem!</span>}
+        
+        {isLoading ? (
+          <div className="flex justify-center mt-5">
+            <Spinner />
+          </div>
+        ) 
+        : <button type="submit" className="bg-orange-500 rounded-xl p-2 text-white cursor-pointer hover:opacity-80 duration-200 ease-linear mt-5">Criar conta</button>}
       </div>
 
       <p className="text-center">
