b6948cfb050cba5c6e802aab9aca323d292dd8f8
joaolucasalves010 <ylucasslalves@gmail.com>
2026-03-09 19:56:50 -0300

    envolvendo context na aplicação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,31 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import AuthLayout from "./components/AuthLayout"
-import AccountCreationSuccess from "./pages/AccountCreationSuccess"
-import ErrorPage from "./pages/ErrorPage"
-import Home from "./pages/Home"
-import CreateProduct from "./pages/CreateProduct"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import AuthLayout from "./components/AuthLayout";
+import AccountCreationSuccess from "./pages/AccountCreationSuccess";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import CreateProduct from "./pages/CreateProduct";
+import { UserProvider } from "./contexts/UserContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/auth/" element={<AuthLayout />}>
-          <Route path="signup" element={<SignUp />}/>
-          <Route path="signin" element={<SignIn />}/>
-          <Route path="success/:username" element={<AccountCreationSuccess />}/>
-        </Route>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth/" element={<AuthLayout />}>
+            <Route path="signup" element={<SignUp />} />
+            <Route path="signin" element={<SignIn />} />
+            <Route
+              path="success/:username"
+              element={<AccountCreationSuccess />}
+            />
+          </Route>
 
-        <Route path="/products/">
-          <Route path="create_product" element={<CreateProduct />} />
-        </Route>
+          <Route path="/products/">
+            <Route path="create_product" element={<CreateProduct />} />
+          </Route>
 
-          <Route path="/" element={<Home />}/>      
-          <Route path="*" element={<ErrorPage />}/>
-      </Routes>
-    </BrowserRouter>
-  )
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
+  );
 }
 
-export default App
+export default App;
