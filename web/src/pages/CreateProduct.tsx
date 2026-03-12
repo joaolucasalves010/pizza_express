be651286aeca0d09bc99cea7487829ac512df25b
joaolucasalves010 <ylucasslalves@gmail.com>
2026-03-12 10:53:38 -0300

    alterando formulário de criação de produtos

--- web/src/pages/CreateProduct.tsx
+++ web/src/pages/CreateProduct.tsx
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
-import Footer from "../components/Footer";
-import { User, ArrowLeft } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { Pizza, PlusCircle } from "lucide-react";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import api from "@/services/api";
+import { Toaster } from "sonner";
+import { toast } from "sonner";
 
 const createProductSchema = z.object({
   name: z.string().min(1, "O nome do produto é obrigatório"),
   price: z.string().min(1, "O preço é obrigatório").refine((val) => parseFloat(val) > 0, "O preço deve ser superior a zero"),
-  description: z.string().min(1, "A descrição é obrigatória"),
+  description: z.string().min(20, "O minimo é 20").max(255, "O máximo é 255"),
   image: z
     .any()
     .refine((files) => files?.length > 0, "A imagem é obrigatória")
@@ -18,7 +19,6 @@ const createProductSchema = z.object({
 type CreateProductFormData = z.infer<typeof createProductSchema>;
 
 const CreateProduct = () => {
-  const navigate = useNavigate();
 
   useEffect(() => {
     document.title = "Cadastrar Produto";
@@ -33,32 +33,39 @@ const CreateProduct = () => {
     resolver: zodResolver(createProductSchema),
   });
 
-  const onSubmit = (data: CreateProductFormData) => {
-    console.log({
-      name: data.name,
-      price: data.price,
-      image: data.image[0],
-      description: data.description,
-    });
+  const onSubmit = async (data: CreateProductFormData) => {
+    try {
+      const product = {
+        "name": data.name,
+        "description": data.description,
+        "price": data.price,
+      }
+      const res = await api.post("/products/", product, {withCredentials: true})
+      const formData = new FormData()
+      formData.append("file", data.image[0])
+
+      api.post(`/products/${res.data.product_id}/images`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        withCredentials: true
+      })
+      toast.success("Produto criado com sucesso!")
+
+    } catch (err: any) {
+      console.log(err)
+    }
   };
 
   return (
-    <div className="min-h-screen flex flex-col bg-orange-100">
+    <div className="flex flex-col bg-gray-200">
       <main className="flex-1 flex items-center justify-center p-6">
-        <div className="w-full max-w-xl bg-white shadow-2xl shadow-black-300 rounded-2xl p-8 relative">
+        <div className="w-full max-w-xl bg-white shadow-2xl shadow-zinc-700 rounded-2xl p-8 border border-zinc-400">
           <div className="flex justify-center items-center">
             <div className="bg-orange-500 w-full rounded-full p-4 max-w-[62px]">
-              <User size={30} className="text-white" />
+              <Pizza size={30} className="text-white" />
             </div>
           </div>
-
-          <button
-            type="button"
-            onClick={() => navigate(-1)}
-            className="border border-gray-400 hover:scale-102 absolute top-3 left-4 p-2 text-black hover:bg-zinc-400 cursor-pointer rounded-full transition"
-          >
-            <ArrowLeft size={20} />
-          </button>
 
           <h1 className="text-3xl font-semibold text-center mb-6">
             Cadastrar Novo Produto
@@ -70,7 +77,7 @@ const CreateProduct = () => {
               <input
                 type="text"
                 placeholder="Ex: Pizza Calabresa"
-                className="border p-3 rounded-xl focus:outline-orange-500"
+                className="border border-zinc-500 p-3 rounded-xl focus:outline-orange-500"
                 {...register("name")}
               />
               {errors.name && <span className="text-red-500 text-sm">{errors.name.message}</span>}
@@ -82,7 +89,7 @@ const CreateProduct = () => {
                 type="number"
                 step="0.01"
                 placeholder="Ex: 39.90"
-                className="border p-3 rounded-xl focus:outline-orange-500"
+                className="border p-3 rounded-xl focus:outline-orange-500 border-zinc-500"
                 {...register("price")}
               />
               {errors.price && <span className="text-red-500 text-sm">{errors.price.message}</span>}
@@ -93,7 +100,7 @@ const CreateProduct = () => {
               <input
                 type="file"
                 accept="image/*"
-                className="border p-2 rounded-xl file:bg-orange-500 file:text-white file:border-none file:px-4 file:py-2 file:rounded-lg file:cursor-pointer"
+                className="border border-zinc-500 p-2 rounded-xl file:bg-orange-500 file:text-white file:border-none file:px-4 file:py-2 file:rounded-lg file:cursor-pointer"
                 {...register("image")}
               />
               {errors.image && <span className="text-red-500 text-sm">{errors.image.message as string}</span>}
@@ -103,7 +110,7 @@ const CreateProduct = () => {
               <label className="font-medium">Descrição</label>
               <textarea
                 placeholder="Descreva os ingredientes ou características do produto..."
-                className="border p-3 rounded-xl focus:outline-orange-500 resize-none"
+                className="border p-3 rounded-xl focus:outline-orange-500 resize-none border-zinc-500"
                 rows={4}
                 {...register("description")}
               />
@@ -112,15 +119,16 @@ const CreateProduct = () => {
 
             <button
               type="submit"
-              className="bg-orange-500 text-white py-3 rounded-xl mt-4 hover:opacity-85 transition duration-200 font-semibold"
+              className="cursor-pointer bg-orange-500 text-white py-3 rounded-xl mt-4 hover:opacity-85 transition duration-200 font-semibold flex items-center justify-center gap-2"
             >
+              <PlusCircle />
               Cadastrar Produto
             </button>
           </form>
         </div>
       </main>
 
-      <Footer />
+      <Toaster />
     </div>
   );
 };
