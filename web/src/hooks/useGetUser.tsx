a37f7328af41bfcd20f01d80e364a70875f2a5fc
joaolucasalves010 <ylucasslalves@gmail.com>
2026-03-12 10:53:19 -0300

    hook getUser

--- web/src/hooks/useGetUser.tsx
+++ web/src/hooks/useGetUser.tsx
@@ -1,10 +1,30 @@
 import api from "@/services/api"
-type Props = {}
 
-const useGetUser = (props: Props) => {
-  return (
-    <div>useGetUser</div>
-  )
+import { UserContext } from "@/contexts/UserContext"
+import { useContext } from "react"
+
+import { useNavigate } from "react-router-dom"
+
+const useGetUser = () => {
+  const { setUser } = useContext(UserContext)!
+  const navigate = useNavigate()
+
+    async function getUser() {
+      try {
+        const res = await api.get("/auth/me", {withCredentials: true})
+
+        if (res.status === 200) {
+          setUser(res.data)
+        }
+      } catch(err: any) {
+        if (err.response?.status === 401) {
+          navigate("/auth/signin")
+      }
+    }
+  }
+
+  return { getUser }
+
 }
 
 export default useGetUser
