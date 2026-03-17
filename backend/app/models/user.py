45374950fd572d7b2a148e4d38a95cd5e6eb5717
joaolucasalves010 <ylucasslalves@gmail.com>
2026-03-16 22:32:02 -0300

    adicionando campo active

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -3,6 +3,7 @@ from sqlmodel import SQLModel, Field
 class UserBase(SQLModel):
     username: str = Field(index=True)
     full_name: str = Field(index=True)
+    active: bool = Field(default=True)
 
 class User(UserBase):
     password: str
