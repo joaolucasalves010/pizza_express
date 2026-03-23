b09a68685c06309937f9f86dbd89e639a38beb3b
joaolucasalves010 <ylucasslalves@gmail.com>
2026-03-23 11:07:34 -0300

    atualizando model de orders

--- backend/app/models/orders.py
+++ backend/app/models/orders.py
@@ -1,7 +1,6 @@
 from sqlmodel import Field, SQLModel
 
 class Orders(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    description: str = Field(index=True)
-    user_name: str = Field(index=True)
+    product_id: int | None = Field(default=None, primary_key=True)
+    quantity: int = Field(default=None)
     client_id: int | None = Field(default=None, foreign_key="usersdb.id")
